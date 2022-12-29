c1bd7f7c8f9e8f704ac0e6d1dce6c3bce0c98585
Matheus Emanoel <matheusemanoel1407@gmail.com>
2022-12-29 11:27:11 -0400

    server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "********@gmail.com",
+    to: "matheusemanoel1407@gmail.com",
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
