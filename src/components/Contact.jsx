9f25e2e2d978c542522edff5fe3c14ec5daec982
Matheus Emanoel <matheusemanoel1407@gmail.com>
2023-02-18 16:18:07 -0400

    upp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,87 +1,37 @@
+import { isVisible } from "@testing-library/user-event/dist/utils";
 import { useState } from "react"
-import { Col, Row, Container } from "react-bootstrap";
-import ContactImg from "../assets/img/contact-img.svg";
-export const Contact = () => {
+import { Container } from "react-bootstrap";
+import { WhatsappAnimation } from "../components/WhatsappAnimation";
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
-    const formInitialDetails = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        message: ''
-      }
-      const [formDetails, setFormDetails] = useState(formInitialDetails);
-      const [buttonText, setButtonText] = useState('Enviar');
-      const [status, setStatus] = useState({});
-    
-      const onFormUpdate = (category, value) => {
-          setFormDetails({
-            ...formDetails,
-            [category]: value
-          })
-      }
-    
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        setButtonText("Enviando...");
-        let response = await fetch("http://localhost:5000/contact", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json;charset=utf-8",
-          },
-          body: JSON.stringify(formDetails),
-        });
-        setButtonText("Enviar");
-        let result = await response.json();
-        setFormDetails(formInitialDetails);
-        if (result.code == 200) {
-          setStatus({ succes: true, message: 'Message sent successfully'});
-        } else {
-          setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
-        }
-      };
-    
+
+import {
+    BrowserRouter as Router
+  } from "react-router-dom";
+  
+export const Contact = () => {
+  
+
+
 
     return (
-        <section className="contact" id='connect'>
-            <Container>
-                <Row className="align-items-center">
-                    <Col md={6}>
-                        <img src={ContactImg} />
-                    </Col>
-                    <Col md={6}>
-                        <h2>Entre em contato</h2>
-                        <form onSubmit={handleSubmit}>
-                            <Row>
-                                <Col sm={6} className='px-1'>
-                                    <input type="text" value={formDetails.firstName} placeholder="Nome" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                                </Col>
-                                <Col sm={6} className='px-1'>
-
-                                    <input type="text" value={formDetails.lastName} placeholder="Sobrenome" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
-                                </Col>
-                                <Col sm={6} className='px-1'>
-                                    <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                                </Col>
-                                <Col sm={6} className='px-1'>
-                                    <input type="tel" value={formDetails.phone} placeholder="Telefone" onChange={(e) => onFormUpdate('phone', e.target.value)} />
-                                </Col>
-                                <Col>
-                                    <textarea row="8" value={formDetails.message} placeholder="Mensagem" onChange={(e) => onFormUpdate('message', e.target.value)} />
-                                    <button type="submit"><span>{buttonText}</span></button>
-                                </Col>
-                                {
-                                    status.message &&
-                                    <Col>
-                                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                                    </Col>
-                                }
-                            </Row>
-                        </form>
-                    </Col>
-                </Row>
-            </Container>
-
+        <Container fluid className="p-0">
+        <section className="contact d-flex flex-row flex-wrap align-content-end justify-content-center align-items-center " id='connect'>
+              <div className="contact-0">
+               <WhatsappAnimation/>
+              </div>
+              <div className="contact1 mx-5">
+                <button> 
+                    <a target="_blank" href="https://api.whatsapp.com/send?phone=5566999719231">
+                    <span>Entre em contato
+                    <WhatsAppIcon/>
+                    </span>
+                    </a> 
+                </button>
+              </div>
+           
         </section>
+        </Container>
+
     )
 }
